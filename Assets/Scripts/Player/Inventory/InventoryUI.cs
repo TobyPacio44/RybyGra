c148c89b0a41ea01fee1a47a319c1719b862bc0d
TobyPacio44 <ppaciowski@gmail.com>
2024-06-05 19:36:41 +0200

    System inventory

---
+++ Assets/Scripts/Player/Inventory/InventoryUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject fishingRod;
+    public List<GameObject> fishesItems = new List<GameObject>();
+    public List<GameObject> itemsItems = new List<GameObject>();
+    public List<GameObject> zanêtyItems = new List<GameObject>();
+    public List<GameObject> przynêtyItems = new List<GameObject>();
+}
