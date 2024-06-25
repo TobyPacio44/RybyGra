b71d6b91251832b1d3a3d76746e3100e37f06781
TobyPacio44 <ppaciowski@gmail.com>
2024-06-26 00:34:33 +0200

    x

--- Assets/Scripts/Fish/FishObject.cs
+++ Assets/Scripts/Fish/FishObject.cs
@@ -6,9 +6,8 @@ public class FishObject : ItemObject
 {
     [Header("(0-17)")]
     public int fishDifficulty;
-    [Header("(0.1 - 500) kg")]
-    [Range(0.1f, 500)]
-    public float weight;
+    //(0.1 - 500) kg
+    float weight;
 
     [Header("Mno¿nik")]
     public int mnoznik;
